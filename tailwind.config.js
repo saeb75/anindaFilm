f9abea8dee780ff6947107c537f371469b3d06b5
saeb75 <saebfat@gmail.com>
2022-01-07 14:13:52 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+
+    extend: {
+      colors: {
+        body: "#191A1F",
+        primary: "#D22F27",
+        secondary: "#2D2D35",
+        lightColor: "#696972",
+        lightGray: "#9696A3",
+        yellow: "#F9D729",
+        white: "#fff",
+      },
+    },
+  },
+  plugins: [],
+};
