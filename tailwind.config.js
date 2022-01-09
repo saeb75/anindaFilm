8d5855d313eac26859bdbb6e75d4ea772f1c77cf
saeb75 <saebfat@gmail.com>
2022-01-08 22:58:20 -0800

    update project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     screens: {
+      sm: "480px",
       sm: "640px",
       // => @media (min-width: 640px) { ... }
 
       md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      lg: "1180px",
+      lg: "1250px",
       // => @media (min-width: 1024px) { ... }
 
       xl: "1280px",
